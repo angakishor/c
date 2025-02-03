b71fc6596ddd5d8ee7e489b9f8ee5b18249e84d6
anga kishor <147230833+angakishor@users.noreply.github.com>
2025-02-03 10:13:20 +0530

    Update minimumelement.c

--- minimumelement.c
+++ minimumelement.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int n=5 ,a[5]={1,2,4,5,3},min;
+    for(int i=1;i<n;i++){
+        min=a[0];
+        if(min>a[i])
+        {
+            min=a[i];
+        }
+    }
+    printf("%d",min);
+    return 0;
+}
